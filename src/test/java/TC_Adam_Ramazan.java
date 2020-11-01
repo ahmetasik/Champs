fd230ed2229630b9f9a077079aad9c05c1008d58
rsevindik <rsuzem@gmail.com>
2020-11-01 15:31:36 -0500

    updated name of the class and new branch

--- src/test/java/TC_Adam_Ramazan.java
+++ src/test/java/TC_Adam_Ramazan.java
@@ -1,7 +1,8 @@
-public class Adam_Ramazan {
+public class TC_Adam_Ramazan {
     public static void main(String[] args) {
         System.out.println("Go Champs");
         //Feel free to make changes dear team lead: Ahmet
         // I just ran the code and i'm able to see the target folder now:))
+        // any commands?
     }
 }
